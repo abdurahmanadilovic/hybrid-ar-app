9abe7b9abeb715dd05764b6bd56453ccba849454
Abdurahman Adilovic <abdurahman.adilovic@gmail.com>
2024-11-18 08:34:01 +0100

    change to remote postaman url

--- data/src/main/java/com/abdu/hybridapp/data/ApiService.kt
+++ data/src/main/java/com/abdu/hybridapp/data/ApiService.kt
@@ -8,6 +8,6 @@ interface ApiService {
     suspend fun getInitialWorldPosition(): LocationDTO
 
     companion object {
-        const val DEV_URL = "http://localhost:8080"
+        const val DEV_URL = "https://58ae66b4-e4e5-44f4-903f-fd0dbd2b117f.mock.pstmn.io"
     }
 }
